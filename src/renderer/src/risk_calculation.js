a4e8cdf1ca5be10c6dd850b0c7368dd1510b9be2
Selim Elkaffas <selimkaffas@gmail.com>
2026-03-26 00:44:33 +0200

    added egfr

--- src/renderer/src/risk_calculation.js
+++ src/renderer/src/risk_calculation.js
@@ -6,16 +6,20 @@ const BETA_COEFFS = {
     hdl: -0.1606857,
     sbpUnder110: -0.2394003,
     sbpOver110: 0.3600781,
+    egfrUnder60: 0.6045917,
+    egfrOver60: 0.0433769,
     diabetes: 0.8667604,
     smoker: 0.5360739,
     statin: -0.1477655,
     Antihtn: 0.3151672,
-    constant: -3.307728,
+    // Interaction terms
     age_nonHdl: -0.0819715,
     age_hdl: 0.0306769,
     age_sbpOver110: -0.0946348,
+    age_egfrUnder60: -0.1637806,
     age_diabetes: -0.27057,
-    age_smoker: -0.078715
+    age_smoker: -0.078715,
+    constant: -3.307728
   },
   men: {
     age: 0.7688528,
@@ -23,23 +27,27 @@ const BETA_COEFFS = {
     hdl: -0.0954431,
     sbpUnder110: -0.4347345,
     sbpOver110: 0.3362658,
+    egfrUnder60: 0.5378979,
+    egfrOver60: 0.0164827,
     diabetes: 0.7692857,
     smoker: 0.4386871,
     statin: -0.1337349,
     Antihtn: 0.288879,
-    constant: -3.031168,
+    // Interaction terms
     age_nonHdl: -0.0517874,
     age_hdl: 0.0191169,
     age_sbpOver110: -0.1049477,
+    age_egfrUnder60: -0.1543702,
     age_diabetes: -0.2251948,
-    age_smoker: -0.0895067
+    age_smoker: -0.0895067,
+    constant: -3.031168
   }
 };
 
 const MGDL_TO_MMOL = 0.02586;
 
 export const calculateRisk = (patient) => {
-    const { age, sex, tChol, hdl, sbp, diabetes, smoker, usingAntihtn, usingStatin } = patient;
+    const { age, sex, tChol, hdl, sbp, bmi, egfr, diabetes, smoker, usingAntihtn, usingStatin } = patient;
 
     const c = sex === 'female' ? BETA_COEFFS.women : BETA_COEFFS.men;
 
@@ -54,12 +62,17 @@ export const calculateRisk = (patient) => {
     const sbpUnder110Scaled = (Math.min(sbp, 110) - 110) / 20;
     const sbpOver110Scaled = (Math.max(sbp, 110) - 130) / 20;
 
+    const egfrUnder60Scaled = (Math.min(egfr, 60) - 60) / -15;
+    const egfrOver60Scaled = (Math.max(egfr, 90) - 60) / -15;
+
     let L = c.constant + 
           (c.age * ageScaled) + 
           (c.nonHdl * nonHdlScaled) +  (c.age_nonHdl * ageScaled * nonHdlScaled) + 
           (c.hdl * hdlScaled) + (c.age_hdl * ageScaled * hdlScaled) + 
           (c.sbpUnder110 * sbpUnder110Scaled) +
-          (c.sbpOver110 * sbpOver110Scaled) + (c.age_sbpOver110 * ageScaled * sbpOver110Scaled);
+          (c.sbpOver110 * sbpOver110Scaled) + (c.age_sbpOver110 * ageScaled * sbpOver110Scaled) +
+          (c.egfrUnder60 * egfrUnder60Scaled) + (c.age_egfrUnder60 * ageScaled * egfrUnder60Scaled) +
+          (c.egfrOver60 * egfrOver60Scaled);
 
     if (diabetes) L += c.diabetes + (c.age_diabetes * ageScaled * 1);
     if (smoker) L += c.smoker + (c.age_smoker * ageScaled * 1);
