41d42e6b42c84d6414627ebb2a4ff1ff56277956
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-03 09:40:45 +0200

    Added patient profile

---
+++ src/renderer/src/PatientProfile.jsx
@@ -0,0 +1,11 @@
+import { useParams } from 'react-router-dom'
+
+function PatientProfile() {
+   // const { id } = useParams();
+
+    return (
+        <h1>Patient Profile</h1>
+    )
+}
+
+export default PatientProfile()
