1e71ff1cc56b7dcf5484e37c10c26a0c34fe4cc1
Selim Elkaffas <selimkaffas@gmail.com>
2026-04-05 15:57:45 +0200

    Added fields for pocketbase integration

--- src/renderer/src/db/db.js
+++ src/renderer/src/db/db.js
@@ -2,6 +2,11 @@ import { Dexie } from "dexie"
 
 export const db = new Dexie("myDatabase")
 db.version(1).stores({
-    patients: "++id, name",
-    snapshots: "++id, patientId, timestamp"
+    patients: "++id, name, pbId, synced",
+    snapshots: "++id, patientId, timestamp, pbId, synced"
 })
+
+// // Delete any associated snapshots when a patient is deleted
+// db.patients.hook('deleting', function (primaryKey, obj, transaction) {
+//     transaction.table('snapshots').where('patientId').equals(Number(primaryKey)).delete()
+// })
